2d1da393eb4fb0b402384a5d0c58de9176cf8549
sawilkhan <68275664+sawilkhan@users.noreply.github.com>
2024-05-30 12:01:59 +0530

    added config file and LoadConfig

--- main.go
+++ main.go
@@ -7,17 +7,19 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/sawilkhan/simple-bank/api"
 	db "github.com/sawilkhan/simple-bank/db/sqlc"
+	"github.com/sawilkhan/simple-bank/db/util"
 )
 
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
 
 func main(){
-	conn, err := sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig(".")
+	if err != nil{
+		log.Fatal("cannot load config")
+	}
+
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil{
 		log.Fatal("cannot establish connection with the database", err)
 	}
@@ -25,7 +27,7 @@ func main(){
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil{
 		log.Fatal("Cannot start server: ", err)
 	}
